2bdc9dda23bb2082d704d24ae363dee39f58fc5b
Abu Wabaidar Hasan Fahim <fahimhasan314@gmail.com>
2024-09-23 12:17:40 +0600

    removed pragma

--- src/Common/MtslErp.Common.Infrastructure/SharedInfrastructureConstants.cs
+++ src/Common/MtslErp.Common.Infrastructure/SharedInfrastructureConstants.cs
@@ -2,8 +2,6 @@ namespace MtslErp.Common.Infrastructure;
 
 public static class SharedInfrastructureConstants
 {
-#pragma warning disable CA2211
-    public static TimeSpan DefaultAbsoluteExpiration = TimeSpan.FromMinutes(10);
-    public static TimeSpan DefaultSlidingExpiration = TimeSpan.FromMinutes(10);
-#pragma warning restore CA2211
+    public static readonly TimeSpan DefaultAbsoluteExpiration = TimeSpan.FromMinutes(10);
+    public static readonly TimeSpan DefaultSlidingExpiration = TimeSpan.FromMinutes(10);
 }
